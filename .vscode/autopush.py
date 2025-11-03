420beb1c13a2f1405ca970c53dd4e75f6d50cb04
hmpmanish <hmpmanish@gmail.com>
2025-11-03 20:56:30 +0530

    Auto update

---
+++ .vscode/autopush.py
@@ -0,0 +1,9 @@
+import os, time
+
+while True:
+    os.chdir(r"C:\Users\Manish Pandey\Desktop\SU\c")
+    os.system("git add .")
+    os.system('git commit -m "Auto update"')
+    os.system("git push origin main")
+    print("✅ Code auto pushed to GitHub!")
+    time.sleep(300)  # 5 minutes wait
