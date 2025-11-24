56360f8acae572e5f6cd210f8a21dfbdb654d6d3
hmpmanish <hmpmanish@gmail.com>
2025-11-25 01:45:53 +0530

    Auto update

--- .vscode/01/01av/01c/02.c
+++ .vscode/01/01av/01c/02.c
@@ -11,26 +11,21 @@ void main() {
 
     printf("\n******Rules for creating variables*****");
 
-    int manishPandey;   
-    manishPandey = 76;  
+    int manishPandey;
+    manishPandey = 76;
 
-    char five = '5';    
+    char five = '5';
 
-    const int i = 9;    
+    const int i = 9;
 
-    manishPandey = 98;  
+    manishPandey = 98;
 
     printf("\n\n******TYPES OF Operators*****\n");
 
     /*
     Arithmetic Operators: +, -, *, /, %
-    Relational Operators: >, <, >=, <=, ==, !=
-    Logical Operators: &&, ||, !
-    Assignment Operators: =, +=, -=, *=, /=, %= 
-    Misc Operators: sizeof, ternary operator (?:)
     */
 
-    // Arithmetic Operators
     int a = 10, b = 7, c = 8;
 
     printf("\nThe sum of a and b is: %d", a + b);
@@ -47,8 +42,8 @@ void main() {
 
     printf("\n(hmp == manish) = %d", hmp == manish);
     printf("\n(hmp != manish) = %d", hmp != manish);
-    printf("\n(hmp > manish)  = %d", hmp > manish);
-    printf("\n(hmp < manish)  = %d", hmp < manish);
+    printf("\n(hmp >  manish) = %d", hmp > manish);
+    printf("\n(hmp <  manish) = %d", hmp < manish);
     printf("\n(hmp >= manish) = %d", hmp >= manish);
     printf("\n(hmp <= manish) = %d", hmp <= manish);
 
@@ -70,59 +65,55 @@ void main() {
     printf("\nA << 2 (Left)     = %d", A << 2);
     printf("\nA >> 2 (Right)    = %d", A >> 2);
 
+    // Assignment Operators
+    int hm = 9;
+    hm += 9;
+    printf("\n hmp is %d", hm);
 
-        // Assignment Operators: =, +=, -=, *=, /=, %= 
-        int hm =9;
-        hm +=9;
-        printf("\n hmp is %d" ,hm);
+    // Input section
+    printf("\n\n ******Taking input from user*****");
 
-        // Misc Operators - * , &
-        // Misc Operators - * , &
-printf("\n\n ******Taking input from user*****");
+    int hmpInput;
 
-int hmpInput;
+    printf("\nPlease enter the value of hmpInput: ");
+    scanf("%d", &hmpInput);
 
-printf("\nPlease enter the value of hmpInput: ");
-// scanf("%d", &hmpInput);   // & lagana zaroori hai
+    printf("\nYou entered %d as hmpInput", hmpInput);
 
-// printf("\nYou entered %d as hmpInput", hmpInput);
-// printf("\nYou entered %f as hmpInput", (float) hmpInput);
-
-// int num1, num2;
-
-// scanf("%d", &num1);
-// scanf("%d", &num2);
-
-// printf("\n num1/num2 is %f", (float) num1 / num2);
-
-
+    // Decision Making
     printf("\n\n******Decision Making in C*****\n\n");
 
-int age;
-printf("Enter your age: ");
-scanf("%d", &age);
+    int age;
+    printf("Enter your age: ");
+    scanf("%d", &age);
 
-if (age >= 21) {
-    printf("You are eligible to vote and drive a car.");
-}
-else if (age >= 18 && age < 21) {
-    printf("You can vote but cannot drive a car.");
-}
-else {
-    printf("You are not eligible to vote or drive.");
-}
-// switch satemnt examplse
-int num;
-printf("Enter a number: ");
-scanf("%d", &num);
+    if (age >= 21) {
+        printf("You are eligible to vote and drive a car.");
+    }
+    else if (age >= 18 && age < 21) {
+        printf("You can vote but cannot drive a car.");
+    }
+    else {
+        printf("You are not eligible to vote or drive.");
+    }
 
-switch(num) {
-    case 1: printf("One"); break;
-    case 2: printf("Two"); break;
-    case 3: printf("Three"); break;
-    default: printf("Invalid");
-}
+    // Switch Statement
+    printf("\n\n******Switch Statement Example*****\n\n");
+
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    switch(num) {
+        case 1: printf("One"); break;
+        case 2: printf("Two"); break;
+        case 3: printf("Three"); break;
+        default: printf("Invalid");
+    }
+
+        printf("\n\n******LOOP IN C*****\n\n");
+        
 
 
-getch();
+    getch();
 }
