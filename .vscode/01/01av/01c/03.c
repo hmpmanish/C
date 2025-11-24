09fab2607eb627d1f5278111d106cbe8438579e8
hmpmanish <hmpmanish@gmail.com>
2025-11-25 02:26:29 +0530

    Auto update

--- .vscode/01/01av/01c/03.c
+++ .vscode/01/01av/01c/03.c
@@ -14,9 +14,16 @@ int sum(int a, int b)
 {
     return a+ b;
 }
+
+
+float average(float a, float b)
+
+{
+    return (a+ b)/2;
+}
 void main()
 {
-    printf("The value is:\n"); // Corrected printf
+    printf("\n the average of 100 and  299 is %f", average(100,299)); // Corrected printf
 
 
     getch(); // Waits for a key press
