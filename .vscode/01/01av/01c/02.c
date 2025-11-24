949c6b3357ea045ae5ecc67ca4949d16e6423bc8
hmpmanish <hmpmanish@gmail.com>
2025-11-24 23:08:35 +0530

    Auto update

--- .vscode/01/01av/01c/02.c
+++ .vscode/01/01av/01c/02.c
@@ -13,7 +13,12 @@ void main() {
 
     int manishPandey;   // variable declaration
     manishPandey = 76;  // variable initialization
-    char five ='5'
+
+    char five = '5'; // variable declaration and initialization
+
+    const int i = 9;  // constant variable
+    // i = 10;  // ❌ ERROR: const value cannot be changed
+    
 
     getch();
 }
