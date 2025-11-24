7b96ffd11bf7c3099df6e2eb67e77bfa50855b89
hmpmanish <hmpmanish@gmail.com>
2025-11-25 01:25:34 +0530

    Auto update

--- .vscode/01/01av/01c/02.c
+++ .vscode/01/01av/01c/02.c
@@ -88,8 +88,17 @@ printf("\nPlease enter the value of hmpInput: ");
 // printf("\nYou entered %d as hmpInput", hmpInput);
 // printf("\nYou entered %f as hmpInput", (float) hmpInput);
 
-int num1, num2;
-scanf("%d", &num1)
+// int num1, num2;
+
+// scanf("%d", &num1);
+// scanf("%d", &num2);
+
+// printf("\n num1/num2 is %f", (float) num1 / num2);
+
+
+    printf("\n******DEcioson making in c*****");
+
+
 
 
     getch();
