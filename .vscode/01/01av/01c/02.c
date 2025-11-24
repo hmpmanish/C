80ee27315ec7523953ede6eace32ae94f69606a2
hmpmanish <hmpmanish@gmail.com>
2025-11-24 23:03:31 +0530

    Auto update

--- .vscode/01/01av/01c/02.c
+++ .vscode/01/01av/01c/02.c
@@ -7,10 +7,13 @@ void main() {
     float b = 10.0;
     char c = 'a';
 
-printf("manish %c", c);
+    printf("manish %c", c);
 
-printf("\n")
-    
+    printf("\n******Rules for creating variables*****");
+
+    int manishPandey;   // variable declaration
+    manishPandey = 76;  // variable initialization
+    char five ='5'
 
     getch();
 }
