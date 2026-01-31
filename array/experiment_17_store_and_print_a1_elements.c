07439e1b0f0a9c9605538c110d89285c1f72c5e6
hmpmanish <hmpmanish@gmail.com>
2026-01-31 11:51:54 +0530

    Auto update

---
+++ array/experiment_17_store_and_print_a1_elements.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int numbers[10];
+
+    // store elements
+    printf("Enter 10 numbers:\n");
+    for (int i = 0; i < 10; i++) {   // ❌ use ; not ,
+        printf("Enter number %d: ", i + 1);
+        scanf("%d", &numbers[i]);
+    }
+
+    // print elements
+    printf("The numbers you entered are:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%d\n", numbers[i]);
+    }
+
+}
